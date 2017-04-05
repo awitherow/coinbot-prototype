03939c4857debd61e53ebe64957f353137dbdb27
awitherow <au.witherow@gmail.com>
2017-04-05 19:52:01 +0200

    chore(priv): add first examples of private functions

--- index.js
+++ index.js
@@ -2,24 +2,31 @@ const gdax = require('gdax');
 const moment = require('moment');
 const client = new gdax.PublicClient();
 
+require('dotenv').config();
+const { KEY, SECRET, PASS, ENDPOINT } = process.env;
+const authClient = new gdax.AuthenticatedClient(KEY, SECRET, PASS, ENDPOINT);
+
 console.log('server started', moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-getRecentActivity();
+activate();
 
-async function getRecentActivity() {
+async function activate() {
     const latestBTCValue = await getSnapshot();
-    console.log(latestBTCValue);
-    // const lastPurchase = getLastPurchaseByUser();
-    // const lastSale = getLastSaleByUser();
-    // if (latestBTCValue < lastSale) run buy algorithm, else getRecentActivity later
-    // if (latestBTCValue > lastPurchase) run sale algorithm, else getRecentActivity later
+    console.log(latestBTCValue.price);
+    const btcAccount = await getAccount('BTC');
+    const usdAccount = await getAccount('USD');
+    console.log(btcAccount, usdAccount);
+    const latestAction = await getAccountHistory(btcAccount.id);
+    console.log(latestAction);
+    // if (latestBTCValue < lastSale) run buy algorithm, else activate later
+    // if (latestBTCValue > lastPurchase) run sale algorithm, else activate later
     // const data = analyze();
     // decide();
 }
 
 function getSnapshot() {
     return new Promise((resolve, reject) =>
-        client.getProductTicker(function(err, res, data) {
+        client.getProductTicker((err, res, data) => {
             if (err) {
                 reject(err);
             } else {
@@ -28,12 +35,28 @@ function getSnapshot() {
         }));
 }
 
-function getLastPurchaseByUser() {
-    // do I want to do this on every getRecentActivity?
-    // or do websocket event?
+function getAccount(type) {
+    return new Promise((resolve, reject) =>
+        authClient.getAccounts((err, res, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data.filter(acct => acct.currency === type)[0]);
+        }));
 }
+
+function getAccountHistory(id) {
+    return new Promise((resolve, reject) =>
+        authClient.getAccountHistory(id, (err, res, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        }));
+}
+
 function getLastSaleByUser() {
-    // do I want to do this on every getRecentActivity?
+    // do I want to do this on every activate?
     // or do websocket event?
 }
 
@@ -46,7 +69,7 @@ function analyze() {
 
 function decide() {
     // based on the velocity and trend direction,
-    // either getRecentActivity again on different intervals,
+    // either activate again on different intervals,
     // or recommendActionToUser() && handleMoneyTheoretically();
 }
 
