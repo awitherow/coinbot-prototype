28e212cfd48acd4658a7ab555d20a75ceb4db852
awitherow <au.witherow@gmail.com>
2017-04-07 23:13:15 +0200

    chore(logger): split concerns

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -2,6 +2,12 @@ const log = console.log;
 const chalk = require('chalk');
 
 module.exports = ({ type, title, info }) =>
-    type === 'error'
-        ? log(chalk.bgYellow.black(`${title}: ${info}`))
-        : log(chalk.inverse(`${title}: ${info}`));
+    type === 'error' ? logError({ title, info }) : logNormal({ title, info });
+
+function logError({ title, info }) {
+    log(chalk.bgYellow.black(`${title}: ${info}`));
+}
+
+function logNormal({ title, info }) {
+    log(chalk.inverse(`${title}: ${info}`));
+}
