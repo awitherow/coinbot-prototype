19062d95c05d377cd292dcd80d06bb29a622ae4e
awitherow <au.witherow@gmail.com>
2017-05-04 14:42:22 +0200

    feat(coinbot): asyncronous iteration

--- src/index.js
+++ src/index.js
@@ -33,7 +33,11 @@ function attemptRun(coins: Array<string>) {
 
     coins.map(async coin => {
         try {
-            await run(coin, currency);
+            const res = await run(coin, currency);
+            if (res instanceof Error) {
+                return new Error(res.message);
+            }
+            reactivate(res, coin);
         } catch (e) {
             logIt({
                 form: 'error',
@@ -48,138 +52,143 @@ function attemptRun(coins: Array<string>) {
 const coins = ['BTC', 'ETH', 'LTC'];
 attemptRun(coins);
 
-// also upon completion, it will be run on a setInterval determined on the
-// decide() function that will be used later.
-async function run(coin: string, currency: string) {
-    logIt({
-        title: 'running at',
-        info: moment().format('MMMM Do YYYY, h:mm:ss a'),
-    });
+type NextRunTimeResponses =
+    | FIVE_MINS_MS
+    | FIFTEEN_MINS_MS
+    | THIRTY_MINS_MS
+    | ONE_HOUR_MS;
 
-    const [marketCoin, myCoinBalance, myCurrencyBalance] = await Promise.all([
-        getSnapshot(`${coin}-${currency}`),
-        getAccount(coin),
-        getAccount(currency),
-    ]);
-
-    if (
-        marketCoin instanceof Error ||
-        myCoinBalance instanceof Error ||
-        myCurrencyBalance instanceof Error
-    ) {
-        return new Error('Getting required gdax info failed.');
-    }
-
-    const lastMatch = await getLastOrder(myCurrencyBalance.id, {
-        coin,
-        currency,
-    });
-
-    // coin -> currency
-    if (Number(parseFloat(myCoinBalance.balance)).toFixed(2) > 0) {
+function run(
+    coin: string,
+    currency: string
+): Promise<NextRunTimeResponses | Error> {
+    return new Promise(async (resolve, reject) => {
         logIt({
-            title: `${coin} balance`,
-            info: parseFloat(myCoinBalance.balance),
+            title: 'running at',
+            info: moment().format('MMMM Do YYYY, h:mm:ss a'),
         });
 
-        console.log(`${coin} -> ${currency}`);
-        if (lastMatch < 0) {
-            const priceAtTimeOfSale =
-                Math.abs(lastMatch) / parseFloat(myCoinBalance.balance);
-            const diffSinceLastTrade =
-                parseFloat(marketCoin.price) - priceAtTimeOfSale;
+        const [
+            marketCoin,
+            myCoinBalance,
+            myCurrencyBalance,
+        ] = await Promise.all([
+            getSnapshot(`${coin}-${currency}`),
+            getAccount(coin),
+            getAccount(currency),
+        ]);
 
-            if (diffSinceLastTrade < -10) {
-                reactivate(ONE_HOUR_MS, coin);
-                logIt({
-                    form: 'error',
-                    title: 'Keep on the look out for potential further investment, Price drop',
-                    info: diffSinceLastTrade,
-                });
-                return;
-            } else if (diffSinceLastTrade > 10) {
-                reactivate(FIFTEEN_MINS_MS, coin);
-                logIt({
-                    form: 'notice',
-                    title: '${coin} price rising, checking more frequently',
-                    info: diffSinceLastTrade,
-                });
-                return;
-            } else if (diffSinceLastTrade > 20) {
-                if (twilioActivated) {
-                    notifyUserViaText(
-                        `SELL ${coin}! Significant difference: ${diffSinceLastTrade}.`
-                    );
-                } else {
-                    logIt({
-                        title: 'Price difference signficant, buy!',
-                        info: diffSinceLastTrade,
-                    });
-                }
-                reactivate(FIVE_MINS_MS, coin);
-                return;
-            } else {
-                logIt({
-                    title: 'Price change not significant',
-                    info: diffSinceLastTrade,
-                });
-                reactivate(THIRTY_MINS_MS, coin);
-                return;
-            }
+        if (
+            marketCoin instanceof Error ||
+            myCoinBalance instanceof Error ||
+            myCurrencyBalance instanceof Error
+        ) {
+            return reject(new Error('Getting required gdax info failed.'));
         }
-    }
 
-    if (parseFloat(myCurrencyBalance.balance) > 1) {
-        logIt({
-            title: `${coin} Balance`,
-            info: parseFloat(myCurrencyBalance.balance),
+        const lastMatch = await getLastOrder(myCurrencyBalance.id, {
+            coin,
+            currency,
         });
 
-        console.log(`${coin} -> ${currency}`);
-        if (lastMatch < 0) {
-            const coinPurchasePrice =
-                parseFloat(myCurrencyBalance.balance) /
-                Math.abs(parseFloat(lastMatch));
-            const diffSinceLastTrade =
-                parseFloat(marketCoin.price) - coinPurchasePrice;
+        // coin -> currency
+        if (Number(parseFloat(myCoinBalance.balance)).toFixed(2) > 0) {
+            logIt({
+                title: `${coin} balance`,
+                info: parseFloat(myCoinBalance.balance),
+            });
 
-            if (diffSinceLastTrade > 10) {
-                reactivate(ONE_HOUR_MS, coin);
-                logIt({
-                    form: 'error',
-                    title: 'You bought bitcoin early. Has risen',
-                    info: diffSinceLastTrade,
-                });
-                return;
-            } else if (diffSinceLastTrade < -10) {
-                reactivate(FIFTEEN_MINS_MS, coin);
-                logIt({
-                    form: 'notice',
-                    title: `${coin} is rising, checking more often now.`,
-                    info: diffSinceLastTrade,
-                });
-                return;
-            } else if (diffSinceLastTrade < -20) {
-                if (twilioActivated) {
-                    notifyUserViaText(
-                        `Buy ${coin}! Significant difference: ${diffSinceLastTrade}.`
-                    );
-                } else {
+            console.log(`${coin} -> ${currency}`);
+            if (lastMatch < 0) {
+                const priceAtTimeOfSale =
+                    Math.abs(lastMatch) / parseFloat(myCoinBalance.balance);
+                const diffSinceLastTrade =
+                    parseFloat(marketCoin.price) - priceAtTimeOfSale;
+
+                if (diffSinceLastTrade < -10) {
                     logIt({
-                        title: 'Price difference signficant, sell!',
+                        form: 'error',
+                        title: 'Keep on the look out for potential further investment, Price drop',
                         info: diffSinceLastTrade,
                     });
+                    return resolve(ONE_HOUR_MS);
+                } else if (diffSinceLastTrade > 10) {
+                    logIt({
+                        form: 'notice',
+                        title: '${coin} price rising, checking more frequently',
+                        info: diffSinceLastTrade,
+                    });
+                    return resolve(FIFTEEN_MINS_MS);
+                } else if (diffSinceLastTrade > 20) {
+                    if (twilioActivated) {
+                        notifyUserViaText(
+                            `SELL ${coin}! Significant difference: ${diffSinceLastTrade}.`
+                        );
+                    } else {
+                        logIt({
+                            title: 'Price difference signficant, buy!',
+                            info: diffSinceLastTrade,
+                        });
+                    }
+                    return resolve(FIVE_MINS_MS);
+                } else {
+                    logIt({
+                        title: 'Price change not significant',
+                        info: diffSinceLastTrade,
+                    });
+                    return resolve(THIRTY_MINS_MS);
                 }
-                reactivate(FIVE_MINS_MS, coin);
-                return;
-            } else {
-                logIt({
-                    title: 'Price change not significant',
-                    info: diffSinceLastTrade,
-                });
-                reactivate(THIRTY_MINS_MS, coin);
-                return;
             }
         }
-    }
+
+        if (parseFloat(myCurrencyBalance.balance) > 1) {
+            logIt({
+                title: `${coin} Balance`,
+                info: parseFloat(myCurrencyBalance.balance),
+            });
+
+            console.log(`${coin} -> ${currency}`);
+            if (lastMatch < 0) {
+                const coinPurchasePrice =
+                    parseFloat(myCurrencyBalance.balance) /
+                    Math.abs(parseFloat(lastMatch));
+                const diffSinceLastTrade =
+                    parseFloat(marketCoin.price) - coinPurchasePrice;
+
+                if (diffSinceLastTrade > 10) {
+                    logIt({
+                        form: 'error',
+                        title: 'You bought bitcoin early. Has risen',
+                        info: diffSinceLastTrade,
+                    });
+                    return resolve(ONE_HOUR_MS);
+                } else if (diffSinceLastTrade < -10) {
+                    logIt({
+                        form: 'notice',
+                        title: `${coin} is rising, checking more often now.`,
+                        info: diffSinceLastTrade,
+                    });
+                    return resolve(FIFTEEN_MINS_MS);
+                } else if (diffSinceLastTrade < -20) {
+                    if (twilioActivated) {
+                        notifyUserViaText(
+                            `Buy ${coin}! Significant difference: ${diffSinceLastTrade}.`
+                        );
+                    } else {
+                        logIt({
+                            title: 'Price difference signficant, sell!',
+                            info: diffSinceLastTrade,
+                        });
+                    }
+                    return resolve(FIVE_MINS_MS);
+                } else {
+                    logIt({
+                        title: 'Price change not significant',
+                        info: diffSinceLastTrade,
+                    });
+                    return resolve(THIRTY_MINS_MS);
+                }
+            }
+        }
+    });
 }
