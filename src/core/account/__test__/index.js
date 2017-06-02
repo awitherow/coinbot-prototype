336682f50eaa770c0fc215ed1ff1f938249e1bfd
awitherow <au.witherow@gmail.com>
2017-06-02 19:33:34 +0200

    test(account): prepare suite

---
+++ src/core/account/__test__/index.js
@@ -0,0 +1,11 @@
+const { getRelevantCoinOrder } = require('./');
+
+const { prepareLastOrder } = require('./');
+
+describe('getRelevantCoinOrder', () => {
+    describe('run LTC-USD-1 scenario', () => {
+        const matches = require('./tapes/LTC-USD-1.json');
+        const currency = 'USD';
+        balance = 0.00000001;
+    });
+});
