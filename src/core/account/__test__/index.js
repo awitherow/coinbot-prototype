4ef38a40a89464deaa6c138f4350a44d9ef93079
awitherow <au.witherow@gmail.com>
2017-06-02 19:33:34 +0200

    test(account): prepare suite

---
+++ src/core/account/__test__/index.js
@@ -0,0 +1,9 @@
+const { getRelevantCoinOrder } = require('./');
+
+describe('getRelevantCoinOrder', () => {
+    describe('run BTC-USD-1 scenario', () => {
+        const matches = require('./tapes/BTC-USD-1.json');
+        const currency = 'USD';
+        balance = 0.00000001;
+    });
+});
