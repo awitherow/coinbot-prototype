f7d17a628f37b4889e5533497fcaa3c97fa24c9c
awitherow <au.witherow@gmail.com>
2017-06-03 14:55:14 +0200

    test(advisor): catch cases for purchase advisory

---
+++ src/core/advisor/index.test.js
@@ -0,0 +1,39 @@
+const { advisePurchase } = require('./');
+
+describe('advisePurchase', () => {
+    const coin = 'LTC';
+
+    it('should not accept missing parameters', () => {
+        expect(advisePurchase()).toEqual({
+            advice: false,
+            message: 'missing parameters',
+        });
+    });
+
+    it('should not advise purchase when price change percentage is on increase', () => {
+        const marketCoin = 21;
+        const openingPrice = 20;
+        expect(advisePurchase(coin, marketCoin, openingPrice)).toEqual({
+            advice: false,
+            message: 'LTC market too high to buy.',
+        });
+    });
+
+    it('should advise purchase when price change percentage has dropped significantly', () => {
+        const marketCoin = 15;
+        const openingPrice = 20;
+        expect(advisePurchase(coin, marketCoin, openingPrice)).toEqual({
+            advice: true,
+            message: `${coin} has dropped 33.333%. Purchase advisable.`,
+        });
+    });
+
+    it('should not advise purchase when price change percentage is insignificant', () => {
+        const marketCoin = 19.9;
+        const openingPrice = 20;
+        expect(advisePurchase(coin, marketCoin, openingPrice)).toEqual({
+            advice: false,
+            message: 'LTC market dropped -0.503%, not yet significant.',
+        });
+    });
+});
