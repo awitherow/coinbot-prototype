b20904fed5c4e11588c6d2ab190dca7f5e7bb71c
awitherow <au.witherow@gmail.com>
2017-06-03 16:10:34 +0200

    chore(helpers): add math number standardizer

--- src/helpers/math.js
+++ src/helpers/math.js
@@ -4,6 +4,11 @@ function roundTo(x: number, to: number): number {
     return parseFloat(Number(x).toFixed(to));
 }
 
+function stdNum(x) {
+    return roundTo(x, 3);
+}
+
 module.exports = {
     roundTo,
+    stdNum,
 };
--- src/helpers/math.test.js
+++ src/helpers/math.test.js
@@ -1,7 +1,13 @@
-const { roundTo } = require('./math');
+const { roundTo, stdNum } = require('./math');
 
 describe('roundTo', () => {
     it('should round a number to the number plus x length in decimals', () => {
         expect(roundTo(5.032363239, 5)).toBe(5.03236);
+    });
+});
+
+describe('stdNum', () => {
+    it('should round numbers to the 3rd decimal point.', () => {
+        expect(stdNum(5.032)).toBe(5.032);
     });
 });
