22e30898946891e2facbf2cb692d1c0014124f63
awitherow <au.witherow@gmail.com>
2017-06-25 15:41:26 +0200

    feat(chatbot): add member help messages

--- src/chatbot/conversations.js
+++ src/chatbot/conversations.js
@@ -1,26 +1,31 @@
-function askForConsent(convo) {
-  convo.say("Just a quick introduction...");
-  convo.say(
-    "My name is Coinbot. I am here to help you keep an eye on cryptocurrency prices."
+const {
+  COINBOT_RELAX,
+  COINBOT_UPGRADE,
+  COINBOT_FEEDBACK,
+  CONSENT_ACCEPT,
+  CONSENT_DENY
+} = require("./constants");
+
+function askForConsent(chat) {
+  chat.say("I don't think we have met before!");
+  chat.say(
+    "My name is Coinbot 🤖 I am here to help you keep an eye on cryptocurrency prices."
   );
-  convo.say(
-    "I will let you know if anything significant opportunities are occurring on the GDAX Exchange."
+  chat.say(
+    "I will let you know if the market is like 📈 or like 📉 according to the GDAX exchange."
   );
-  convo.say(
-    "If you would like to learn more about the GDAX Exchange, check here:"
-  );
-  convo.say({
-    text: "I am going to send you text messages about",
+  chat.say({
+    text: "Do you want to me to keep you up to date via Messenger?",
     buttons: [
       {
         type: "postback",
         title: "👍",
-        payload: "CONSENT_ACCEPT"
+        payload: CONSENT_ACCEPT
       },
       {
         type: "postback",
         title: "👎",
-        payload: "CONSENT_DENY"
+        payload: CONSENT_DENY
       }
     ]
   });
@@ -28,15 +33,46 @@ function askForConsent(convo) {
 
 function handleConsentDeny(chat) {
   chat.say(
-    "Hmm, sorry to hear this. Chat with me any time to change your mind!"
+    "Hmm, sorry to hear this. Chat with me any time to change your mind! 👋"
   );
 }
 
 function handleConsentAccept(chat) {
   chat.say("Super excited to have you onboard! 😎");
+  chat.say("I will let you know of significant coin moovement! Enjoy!");
   chat.say(
-    "I will let you know if anything significant is going on! Enjoy ! 😎"
+    "If you need help anytime, just type 'help', or 'info' if you want to learn about me."
   );
+  chat.say(
+    "Last thing... If you think I do a good job, you can learn how to support my growth by typing 'premium' ❤️ "
+  );
+  chat.say("Hope to hear from you soon! 🙇");
+}
+
+function offerBasicHelp(chat) {
+  chat.say(
+    "Type 'help' if you need something specific or 'info' for more info."
+  );
+  chat.say({
+    text: "Or pick the options below:",
+    buttons: [
+      {
+        type: "postback",
+        title: "Take a break",
+        payload: COINBOT_RELAX
+      },
+      {
+        type: "postback",
+        title: "Give feedback",
+        payload: COINBOT_FEEDBACK
+      },
+      {
+        type: "postback",
+        title: "Go Premium",
+        payload: COINBOT_UPGRADE
+      }
+    ]
+  });
 }
 
 module.exports = {
--- src/chatbot/index.js
+++ src/chatbot/index.js
@@ -10,19 +10,28 @@ const bot = new BootBot({
 const {
   askForConsent,
   handleConsentDeny,
-  handleConsentAccept
+  handleConsentAccept,
+  offerBasicHelp
 } = require("./conversations");
+
+const {
+  CONSENT_DENY,
+  CONSENT_ACCEPT,
+  COINBOT_RELAX,
+  COINBOT_FEEDBACK,
+  COINBOT_UPGRADE
+} = require("./constants");
 
 // mock dummy db, need to consider DB solution.
 class Users {
-  get = () => {};
-  set = () => {};
+  get() {}
+  set() {}
 }
 
 // will uncomment later and do in another branch
 // class Events {
-//     store = event => {};
-//     fire = event => {
+//     store(event) {};
+//     fire(event) {
 //         this.store({
 //             ...event,
 //             timestamp: new Date.now(),
@@ -35,9 +44,9 @@ bot.on("message", async (payload, chat) => {
   const { message, sender } = payload;
   const { user } = await chat.getUserProfile();
 
-  chat.say(`Hello, ${user.first_name}! 🙂`);
+  chat.say(`Hello, ${user.first_name}!`);
 
-  const store = users.get(sender.id);
+  const store = Users.get(sender.id);
 
   if (store) {
     // Events.fire({
@@ -45,10 +54,14 @@ bot.on("message", async (payload, chat) => {
     //     category: "message",
     //     event: "message from member",
     // });
-    // give them options for what a member can do
+
+    chat.say("Good to hear from you again. 😌");
+    // offer basic help
+    // check store id, if premium (paying), offer settings
+    offerBasicHelp(chat);
   } else {
-    //user has not registered, ask them to create a unique identifier
-    chat.conversation(convo => askForConsent(convo));
+    // user has not registered, ask them to create a unique identifier
+    askForConsent(chat);
   }
 });
 
@@ -62,17 +75,43 @@ bot.on("postback", async (payload, chat) => {
   // });
 
   switch (payload) {
-    case "CONSENT_DENY":
+    case CONSENT_DENY:
       handleConsentDeny(chat);
       return;
-    case "CONSENT_ACCEPT":
+    case CONSENT_ACCEPT:
       handleConsentAccept(chat);
       Users.set({
         user,
         id: payload.sender.id
       });
       return;
+    case COINBOT_UPGRADE:
+      chat.say("Upgrade coming soon...");
+      // payment conversation
+      // set user as paying
+      return;
+    case COINBOT_RELAX:
+      chat.say("Relax coming soon...");
+      // update user to not get notified
+      // 1 hour, 1 day, 1 week, custom
+      return;
+    case COINBOT_FEEDBACK:
+      chat.say("Feedback coming soon...");
+      // accept feedback message
+      // offer a smiley face system
+      // plus text
+      return;
   }
+});
+
+bot.hear(["info"], (payload, chat) => {
+  chat.say("more info coming soon...");
+});
+bot.hear(["premium"], (payload, chat) => {
+  chat.say("premium coming soon...");
+});
+bot.hear(["help"], (payload, chat) => {
+  chat.say("help coming soon...");
 });
 
 bot.start();
