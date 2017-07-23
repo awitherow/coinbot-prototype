da35fa1af66944f6a021d4238fe5ed686cb35a05
awitherow <au.witherow@gmail.com>
2017-07-23 16:55:18 +0200

    chore(data): enable tracking of coin data

--- src/coinbot/core/tracker.js
+++ src/coinbot/core/tracker.js
@@ -1,6 +1,17 @@
 const jsonfile = require("jsonfile");
+require("dotenv").config();
+
+var path = require("path");
+var appDir = path.dirname(require.main.filename);
 
 module.exports = (id, payload) => {
-  const file = jsonfile.readFileSync("src/db/coinData/data.json");
-  writeFileSync(payload);
+  if (!process.env.TESTING) {
+    const fileName = `${appDir}/db/coinData/${payload.coinCurrency}.json`;
+    let data = jsonfile.readFileSync(fileName);
+    if (!data) {
+      data = [];
+    }
+    data.push(payload);
+    jsonfile.writeFileSync(fileName, data);
+  }
 };
